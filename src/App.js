033ada7b322358658496fca22350c5f623e081eb
Emon Sheikh <sheikhemon@gmail.com>
2021-10-14 17:07:55 +0600

    react bootstrap card with API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Row } from 'react-bootstrap';
+import { useEffect, useState } from 'react';
+import News from './Components/News/News';
 
 function App() {
+  const [news, setNews] = useState([])
+  useEffect(() => {
+    fetch('https://newsapi.org/v2/everything?q=tesla&from=2021-09-14&sortBy=publishedAt&apiKey=095537dcfd164b3d97e6fbb59033f74d')
+      .then(res => res.json())
+      .then(data => setNews(data.articles))
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Row xs={1} md={3} className="g-4">
+        {
+          news.map(nw => <News news={nw}></News>)
+        }
+      </Row>
     </div>
   );
 }
